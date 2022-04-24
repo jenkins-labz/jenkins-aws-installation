7bdaf86a3aa3a824908b1b0ea7a9cc3271ac5458
Amaury Borges Souza <amaurybsouza@gmail.com>
2022-04-23 22:11:49 -0300

    update

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  shared_credentials_file = "/home/amaury/.aws/credencials"
+  profile = "sre"
+}
+
+resource "aws_instance" "tutorials" {
+  ami           = var.ami
+  instance_type = var.instance_type
+}
